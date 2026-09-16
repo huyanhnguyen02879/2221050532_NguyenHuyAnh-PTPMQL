b5a7926484876ae14fea6489944a7afff0af8758
huyanhnguyen02879 <huyanhnguyen2605@gmail.com>
2026-09-16 23:53:05 +0700

    BTH6 bootstrap

--- BTH6/obj/Debug/net10.0/BTH6.AssemblyInfo.cs
+++ BTH6/obj/Debug/net10.0/BTH6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTH6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9415e963287edace9e2e03882d848c1e6bab3ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce1614dafd773502c80c2483dba29bfb985dcbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTH6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTH6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
