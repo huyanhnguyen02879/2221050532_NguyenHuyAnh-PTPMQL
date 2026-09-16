a041787a785ab0bde378bbcddedec78f1d8d3a9c
huyanhnguyen02879 <huyanhnguyen2605@gmail.com>
2026-09-17 00:04:43 +0700

    03/09 lesson4: gui nhan du lieu giua Controller và View: submit form va ViewBag/ViewData thong qua Model

--- DemoMvc/obj/Debug/net10.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net10.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ffd99ec29e7098b8a09dfe8e60acb9b7495dff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc8b8af49a2fda0ed9ba870564c3fb601db4c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
